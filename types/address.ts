7080ded77d25d510c7d6bfe32f13e87ff377d151
¨Nataniel¨ <contatonatanielsoares@gmail.com>
2026-02-20 14:38:24 -0300

    create address type

---
+++ types/address.ts
@@ -0,0 +1,10 @@
+export type Address = {
+    id: string;
+    fullName: string;
+    cpf: string;
+    street: string;
+    number: string;
+    city: string;
+    state: string;
+    zipCode: string;
+}
