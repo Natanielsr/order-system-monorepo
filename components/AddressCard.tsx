5dda20d4eb31b42ad3a261f0e70ffec221f21345
¨Nataniel¨ <contatonatanielsoares@gmail.com>
2026-02-23 14:14:19 -0300

    remove duplicat text-sm

--- components/AddressCard.tsx
+++ components/AddressCard.tsx
@@ -32,7 +32,7 @@ export default function AddressCard({ address }: AddressCardProps) {
         <p className="mt-3 text-sm font-bold text-gray-800">{address.fullName}</p>
         <p className="text-sm font-medium text-gray-800">CPF: {address.cpf}</p>
         <p className="text-sm font-medium text-gray-800">{address.street}, {address.number}</p>
-        <p className="text-sm text-gray-500 text-sm">{address.neighborhood} — {address.city}, {address.state}</p>
+        <p className="text-sm text-gray-500">{address.neighborhood} — {address.city}, {address.state}</p>
         <p className="text-xs text-gray-400 mt-1">CEP: {address.zipCode}</p>
     </div>;
 }
