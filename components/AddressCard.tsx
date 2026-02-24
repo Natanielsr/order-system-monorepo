13a2308c8a6e248206f7837b1010d2b7ea0629e9
¨Nataniel¨ <contatonatanielsoares@gmail.com>
2026-02-24 11:44:27 -0300

    add a handle to edit address in address card

--- components/AddressCard.tsx
+++ components/AddressCard.tsx
@@ -1,14 +1,22 @@
 import { Address } from "@/types/address";
 import { Edit2 } from "lucide-react";
+import { useRouter } from "next/navigation";
 
 interface AddressCardProps {
     address: Address
 }
 
 export default function AddressCard({ address }: AddressCardProps) {
+
+    const router = useRouter();
+    const handleEdit = () => {
+        router.push("/address?id=" + address.id);
+    };
+
+
     return <div className="bg-white p-4 rounded-xl border border-gray-200 shadow-sm hover:border-blue-300 transition-colors group">
         <div className="flex justify-between items-start">
-            {address.default ?
+            {address.isDefault ?
                 (
                     <span className="text-xs font-bold uppercase tracking-wider text-blue-500 bg-blue-50 px-2 py-0.5 rounded">
                         Principal
@@ -24,7 +32,7 @@ export default function AddressCard({ address }: AddressCardProps) {
 
 
             <div className="flex gap-2 opacity-0 group-hover:opacity-100 transition-opacity">
-                <button className="cursor-pointer p-1 text-gray-400 hover:text-blue-600" title="Editar">
+                <button onClick={handleEdit} className="cursor-pointer p-1 text-gray-400 hover:text-blue-600" title="Editar">
                     <Edit2 size={14} />
                 </button>
             </div>
