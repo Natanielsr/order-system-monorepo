da4720def65bc5c5f397b45dd290ef6a00564713
¨Nataniel¨ <contatonatanielsoares@gmail.com>
2026-02-27 15:16:54 -0300

    remove console log from production build, show only console.error

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  compiler: {
+    // Remove todos os console.* em produção, exceto console.error
+    removeConsole: process.env.NODE_ENV === "production" ? { exclude: ["error"] } : false,
+  },
 };
 
 export default nextConfig;
